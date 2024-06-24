1ddd06f4fe4b9086abf661a3872411b058e7a414
Enduriel <endur1el@protonmail.com>
2024-06-24 11:38:45 +0200

    fix: stash size increasing wrapping around indices
    this doesn't happen for town shop because the code
    already does this, which is why I didn't catch it in testing

--- ui/mods/mod_streamlined_ui/hooks/normal/screens/character_screen_inventory_list_module.js
+++ ui/mods/mod_streamlined_ui/hooks/normal/screens/character_screen_inventory_list_module.js
@@ -1,3 +1,14 @@
+SUI.Hooks.CharacterScreenInventoryListModule_createItemSlots = CharacterScreenInventoryListModule.prototype.createItemSlots;
+CharacterScreenInventoryListModule.prototype.createItemSlots = function (_owner, _size, _itemArray, _itemContainer) {
+	var oldCreateItemSlot = this.createItemSlot;
+	this.createItemSlot = function (_owner, _index, _parentDiv, _screenDiv) {
+		return oldCreateItemSlot.call(this, _owner, _itemArray.length, _parentDiv, _screenDiv)
+	}
+	var ret = SUI.Hooks.CharacterScreenInventoryListModule_createItemSlots.call(this, _owner, _size, _itemArray, _itemContainer);
+	this.createItemSlot = oldCreateItemSlot;
+	return ret
+}
+
 CharacterScreenInventoryListModule.prototype.SUI_createItemSlots = SUI.Generic.createItemSlots(CharacterScreenInventoryListModule.prototype.createItemSlots);
 CharacterScreenInventoryListModule.prototype.SUI_destroyItemSlots = SUI.Generic.destroyItemSlots();
 CharacterScreenInventoryListModule.prototype.createDIV = SUI.Generic.createDIV(CharacterScreenInventoryListModule.prototype.createDIV);
--- ui/mods/mod_streamlined_ui/hooks/normal/screens/tactical_combat_result_screen_loot_panel.js
+++ ui/mods/mod_streamlined_ui/hooks/normal/screens/tactical_combat_result_screen_loot_panel.js
@@ -1,3 +1,14 @@
+SUI.Hooks.TacticalCombatResultScreenLootPanel_createItemSlots = TacticalCombatResultScreenLootPanel.prototype.createItemSlots;
+TacticalCombatResultScreenLootPanel.prototype.createItemSlots = function (_owner, _size, _itemArray, _itemContainer) {
+	var oldCreateItemSlot = this.createItemSlot;
+	this.createItemSlot = function (_owner, _index, _parentDiv, _screenDiv) {
+		return oldCreateItemSlot.call(this, _owner, _itemArray.length, _parentDiv, _screenDiv)
+	}
+	var ret = SUI.Hooks.TacticalCombatResultScreenLootPanel_createItemSlots.call(this, _owner, _size, _itemArray, _itemContainer);
+	this.createItemSlot = oldCreateItemSlot;
+	return ret
+}
+
 TacticalCombatResultScreenLootPanel.prototype.SUI_createItemSlots = SUI.Generic.createItemSlots(TacticalCombatResultScreenLootPanel.prototype.createItemSlots);
 TacticalCombatResultScreenLootPanel.prototype.SUI_destroyItemSlots = SUI.Generic.destroyItemSlots();
 TacticalCombatResultScreenLootPanel.prototype.createDIV = SUI.Generic.createDIV(TacticalCombatResultScreenLootPanel.prototype.createDIV);
