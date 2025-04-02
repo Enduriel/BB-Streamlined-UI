38c5f912caf52dd145aeb18653b2d813d626744e
Enduriel <endur1el@protonmail.com>
2025-04-02 14:14:01 +0200

    fix: caching status effect which cannot be cleanly hashed
    this is because it is first removed in removeListBrotherStatusEffects
    and only then added in assignListBrotherStatusEffect.
    This would require a more annoying two stage caching system

--- ui/mods/mod_streamlined_ui/hooks/normal/controls/brother.js
+++ ui/mods/mod_streamlined_ui/hooks/normal/controls/brother.js
@@ -2,5 +2,4 @@ SUI.Generic.wrapWithCache('assignListBrotherImage');
 SUI.Generic.wrapWithCache('showListBrotherLockImage');
 SUI.Generic.wrapWithCache('showListBrotherMoodImage');
 SUI.Generic.wrapWithCache('assignListBrotherDailyMoneyCost');
-SUI.Generic.wrapWithCache('assignListBrotherStatusEffect');
 SUI.Generic.wrapWithCache('assignListBrotherName');
